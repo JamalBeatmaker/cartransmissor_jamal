e75ae53045dd2e3869dcf7ad6fae686c8e2e70ef
JamalBeatmaker <89939812+JamalBeatmaker@users.noreply.github.com>
2022-07-30 19:22:28 -0300

    Atualização pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,9 +10,19 @@ basic.forever(function () {
         radio.sendValue("D", 0)
     }
     if (input.isGesture(Gesture.TiltLeft)) {
-    	
+        hackbitmotors.MotorRunDual(
+        hackbitmotors.Motors.M1A,
+        0,
+        hackbitmotors.Motors.M1A,
+        150
+        )
     }
     if (input.isGesture(Gesture.TiltRight)) {
-    	
+        hackbitmotors.MotorRunDual(
+        hackbitmotors.Motors.M1A,
+        150,
+        hackbitmotors.Motors.M1A,
+        0
+        )
     }
 })
